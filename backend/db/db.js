0047f0d69504becb0245b1d197d304dbed29e5d3
Shivam Raikar <shivamraikar2003@gmail.com>
2024-07-10 19:34:19 +0530

    login route done

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose")
 mongoose.set("strictQuery",false);
 
-const mongoUrl = "mongodb+srv://certifiedcoders:wLDQ5WjysaWfUIz0@eventmanager.lxtd08i.mongodb.net/?retryWrites=true&w=majority&appName=EventManager"
+const mongoUrl = "mongodb+srv://certifiedcoders:wLDQ5WjysaWfUIz0@eventmanager.lxtd08i.mongodb.net/manager"
 
 main().catch((err) => console.log(err))
 
     async function main(){
         await mongoose.connect(mongoUrl);
     }
+
+    module.exports = main;
